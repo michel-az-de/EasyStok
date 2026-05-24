1f8668e745dc3c0d048818fe6442810c3ba5ecd9
Felipe Azevedo <felipe.azevedo@gmail.com>
2026-05-24 10:38:01 -0300

    feat(TASK-EZ-006): EF configs + DbSets WebhookProcessado/CheckoutIdempotency

--- EasyStock.Infra.Postgre/Data/EasyStockDbContext.cs
+++ EasyStock.Infra.Postgre/Data/EasyStockDbContext.cs
@@ -154,6 +154,8 @@ namespace EasyStock.Infra.Postgre.Data
         // Storefront aggregates (ver ADR-0002 multi-tenancy, ADR-0014 vaga lifecycle)
         public DbSet<EasyStock.Domain.Entities.Storefront.Storefront> Storefronts { get; set; } = null!;
         public DbSet<EasyStock.Domain.Entities.Storefront.CardapioItem> CardapioItens { get; set; } = null!;
+        public DbSet<EasyStock.Domain.Entities.Storefront.WebhookProcessado> WebhooksProcessados { get; set; } = null!;
+        public DbSet<EasyStock.Domain.Entities.Storefront.CheckoutIdempotency> CheckoutsIdempotency { get; set; } = null!;
         public DbSet<Categoria> Categorias { get; set; } = null!;
         public DbSet<Produto> Produtos { get; set; } = null!;
         public DbSet<ProdutoVariacao> ProdutosVariacao { get; set; } = null!;
